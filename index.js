1afccae163090a230458147a45dd3c6ecdb05fbc
wz <wz@wzdeMacBook-Pro.local>
2018-06-04 18:50:09 +0800

    删除多余代码

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {LocaleProvider, DatePicker, message} from 'antd';
-// 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
 import './index.css'
-
-moment.locale('zh-cn');
 
 import {Tree, Icon, Popconfirm} from 'antd';
 
